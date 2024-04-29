9715251f1a7524185e6ef9c65fbbc0d71ae20ace
AnyueWang <moonwong.way@gmail.com>
2024-04-29 17:45:47 +0200

    v0.3 Build display controller object v0.3 The front-end display is synergetic with the game logic. In this version, we build display controller object. Display controller's methods:     - displayBoard: display the board of grids.     - updateForm: build the interaction between end user and the game. Some methods are updated: Gameflow's methods:     - gameStart: start the game.     - setPlayers: set players who join the game.     - getCurrentPlayer: get the player who needs to make move at this round.     - currentPlayerMakeMove: the current player makes its move on the gameboard.     - gameRestart (DELETED): restart the game.     - gameIsStart (NEW): check if the game starts.     - getWinner (NEW): return the winner. Gameboard's methods:     - currentBoard: return the current status of the gameboard.     - markOnBoard: set marks on the gameboard according to players' moves.     - checkWinner: check who is the winner based on the current status.     - clearBoard: clear all the marks on the gameboard (reinitialization).     - getTieIndex: get the index of a tie result when the game is over.     - checkAxisOccupied (NEW): check if the pointed grid is marked.

--- index.js
+++ index.js
@@ -1,147 +1,198 @@
-const gameboard = (function () {
-    const nRows = 3, nCols = 3;
-    const indexEmpty = -1, indexTie = 0;
-    const board = ( ()=> {
-        let arr = Array();
-        for (let row = 0; row < nRows; row++) {
-            arr.push(Array());
-            for (let col = 0; col < nCols; col++) {
-                arr[row].push(indexEmpty);
+const main = function () {
+    const gameboard = (function () {
+        const nRows = 3, nCols = 3;
+        const indexEmpty = -1, indexTie = 0;
+        const board = (() => {
+            let arr = Array();
+            for (let row = 0; row < nRows; row++) {
+                arr.push(Array());
+                for (let col = 0; col < nCols; col++) {
+                    arr[row].push(indexEmpty);
+                }
             }
+            return arr
+        })();
+        const currentBoard = () => board;
+        const getTieIndex = () => indexTie;
+        const markOnBoard = (player, axis) => {
+            const index = player.getIndex();
+            board[axis.row][axis.col] = index;
+        };
+        const isWinArr = (arr) => {
+            if (arr.includes(indexEmpty)) return false
+            for (let i = 0; i < arr.length - 1; i++) {
+                if (arr[i] !== arr[i + 1]) return false
+            }
+            return true
+        };
+        const checkWinner = () => {
+            for (let row = 0; row < nRows; row++) {
+                const arrRow = board[row];
+                if (isWinArr(arrRow)) return arrRow[0]
+            }
+            for (let col = 0; col < nCols; col++) {
+                const arrCol = board.map(eachRow => eachRow[col]);
+                if (isWinArr(arrCol)) return arrCol[0]
+            }
+            const arrCrossDown = board.map((eachRow, rowIndex) => eachRow[rowIndex]);
+            if (isWinArr(arrCrossDown)) return arrCrossDown[0]
+            const arrCrossUp = board.map((eachRow, rowIndex) => eachRow[nCols - rowIndex - 1]);
+            if (isWinArr(arrCrossUp)) return arrCrossUp[0]
+            for (let row = 0; row < nRows; row++) {
+                if (board[row].includes(indexEmpty)) return indexEmpty
+            }
+            return indexTie
+        };
+        const clearBoard = function () {
+            for (let row = 0; row < nRows; row++) {
+                for (let col = 0; col < nCols; col++) {
+                    if (board[row] !== indexEmpty) board[row][col] = indexEmpty;
+                }
+            }
+        };
+        const checkAxisOccupied = (axis) => board[axis.row][axis.col] !== indexEmpty;
+        return {
+            currentBoard,
+            markOnBoard,
+            checkWinner,
+            clearBoard,
+            getTieIndex,
+            checkAxisOccupied,
         }
-        return arr
+    })();
+    function createAxis(row, col) {return { row, col }}
+    function createPlayer(name, index,) {
+        const getName = () => name;
+        const getIndex = () => index;
+        const makeMove = function (axis) { gameboard.markOnBoard(this, axis) };
+        return { getName, getIndex, makeMove }
+    }
+    const gameFlow = (function () {
+        let isStart = false;
+        let firstPlayer, secondPlayer, currentPlayer, winner;
+        const setPlayers = (p1, p2) => {
+            firstPlayer = p1;
+            secondPlayer = p2;
+        };
+        const setCurrentPlayer = p => currentPlayer = p;
+        const getCurrentPlayer = () => currentPlayer;
+        const toggleCurrentPlayer = () => {
+            if (currentPlayer === firstPlayer) currentPlayer = secondPlayer
+            else currentPlayer = firstPlayer
+        };
+        const gameStart = () => {
+            isStart = true;
+            winner = undefined;
+            gameboard.clearBoard();
+            setCurrentPlayer(firstPlayer);
+        };
+        const checkResult = () => {
+            const winnerIndex = gameboard.checkWinner();
+            if (winnerIndex === firstPlayer.getIndex()) {
+                winner = firstPlayer;
+                isStart = false;
+            }
+            if (winnerIndex === secondPlayer.getIndex()) {
+                winner = secondPlayer;
+                isStart = false;
+            }
+            if (winnerIndex === gameboard.getTieIndex()) {
+                isStart = false;
+            }
+        };
+        const currentPlayerMakeMove = (axis) => {
+            currentPlayer.makeMove(axis);
+            checkResult();
+            if (isStart) { toggleCurrentPlayer() }
+        };
+        const gameIsStart = () => isStart;
+        const getWinner = () => winner;
+        return {
+            gameStart,
+            setPlayers,
+            getCurrentPlayer,
+            currentPlayerMakeMove,
+            gameIsStart,
+            getWinner,
+        }
+    })();
+    const displayController = (function () {
+        const boardContainer = document.querySelector(".board-container");
+        const btnStart = document.querySelector("#btn-start");
+        const instruction = document.querySelector("h2");
+        const displayBoard = () => {
+            const board = gameboard.currentBoard();
+            for (let row = 0; row < board.length; row++) {
+                for (let col = 0; col < board[row].length; col++) {
+                    const newButton = document.createElement("button");
+                    newButton.classList.add("board-grid");
+                    newButton.setAttribute("row", row);
+                    newButton.setAttribute("col", col);
+                    boardContainer.appendChild(newButton);
+                }
+            }
+        };
+        const updateMoveInstruction = (inst, player) => {
+            inst.textContent = `${player.getName()}, please make your move 😀!`;
+        };
+        const clearBoard = () => {
+            const btnGrids = document.querySelectorAll(".board-grid");
+            btnGrids.forEach((btn => {
+                btn.textContent = "";
+            }));
+        }
+        const enableGridBtns = () => {
+            const btnGrids = document.querySelectorAll(".board-grid");
+            btnGrids.forEach((btn) => {
+                const row = btn.getAttribute("row");
+                const col = btn.getAttribute("col");
+                const axis = createAxis(row, col);
+                btn.addEventListener("pointerover", () => {
+                    if (!gameboard.checkAxisOccupied(axis) && gameFlow.gameIsStart()) {
+                        btn.classList.add("board-grid-hover");
+                        btn.textContent = gameFlow.getCurrentPlayer().getIndex();
+                    }
+                });
+                btn.addEventListener("pointerleave", () => {
+                    if (!gameboard.checkAxisOccupied(axis) && gameFlow.gameIsStart()) {
+                        btn.classList.remove("board-grid-hover");
+                        btn.textContent = "";
+                    }
+                });
+                btn.addEventListener("click", () => {
+                    if (!gameboard.checkAxisOccupied(axis) && gameFlow.gameIsStart()) {
+                        btn.classList.remove("board-grid-hover");
+                        gameFlow.currentPlayerMakeMove(axis);
+                        updateMoveInstruction(instruction, gameFlow.getCurrentPlayer());
+                        if (!gameFlow.gameIsStart()) {
+                            const winner = gameFlow.getWinner();
+                            if (!!winner) { instruction.textContent = `Game over! 🎉 Winner is ${winner.getName()}!` }
+                            else { instruction.textContent = "Game over! 🫱🏼‍🫲🏾 It's a tie!" }
+                        }
+                    }
+                });
+            });
+        }
+        const updateForm = () => {
+            btnStart.addEventListener("click", () => {
+                const namePlayer1 = document.querySelector("#name-player1").value;
+                const namePlayer2 = document.querySelector("#name-player2").value;
+                const player1 = createPlayer(namePlayer1, "⭕️");
+                const player2 = createPlayer(namePlayer2, "❌");
+                clearBoard();
+                gameFlow.setPlayers(player1, player2);
+                gameFlow.gameStart();
+                btnStart.textContent = "Restart";
+                updateMoveInstruction(instruction, gameFlow.getCurrentPlayer());
+                enableGridBtns();
+            });
+        }
+        return {
+            displayBoard,
+            updateForm,
+        }
     })();
 
-    const currentBoard = () => board;
-    const getTieIndex = () => indexTie;
-    const markOnBoard =  (player, axis)=> {
-        const index = player.getIndex();
-        board[axis.row][axis.col] = index;
-    };
-    const isWinArr =  (arr)=> {
-        if (arr.includes(indexEmpty)) return false
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] !== arr[i + 1]) return false
-        }
-        return true
-    };
-    const checkWinner =  () =>{
-        for (let row = 0; row < nRows; row++) {
-            const arrRow = board[row];
-            if (isWinArr(arrRow)) return arrRow[0]
-        }
-        for (let col = 0; col < nCols; col++) {
-            const arrCol = board.map(eachRow => eachRow[col]);
-            if (isWinArr(arrCol)) return arrCol[0]
-        }
-        const arrCrossDown = board.map((eachRow, rowIndex) => eachRow[rowIndex]);
-        if (isWinArr(arrCrossDown)) return arrCrossDown[0]
-        const arrCrossUp = board.map((eachRow, rowIndex) => eachRow[nCols - rowIndex - 1]);
-        if (isWinArr(arrCrossUp)) return arrCrossUp[0]
-        for (let row = 0; row < nRows; row++) {
-            if (board[row].includes(indexEmpty)) return indexEmpty
-        }
-        return indexTie
-    };
-    const clearBoard = function () {
-        for (let row = 0; row < nRows; row++) {
-            for (let col = 0; col < nCols; col++) {
-                if (board[row] !== indexEmpty) board[row][col] = indexEmpty;
-            }
-        }
-    };
-
-    return {
-        currentBoard,
-        markOnBoard,
-        checkWinner,
-        clearBoard,
-        getTieIndex,
-    }
-})();
-
-function createAxis(row, col) {
-    return { row, col }
-}
-
-function createPlayer(name, index,) {
-    const getName = () => name;
-    const getIndex = () => index;
-    const makeMove = function (axis) { gameboard.markOnBoard(this, axis) };
-
-    return { getName, getIndex, makeMove }
-}
-
-const gameFlow = (function () {
-    let isStart = false;
-    let firstPlayer, secondPlayer, currentPlayer, winner;
-    let resultString;
-
-    const setPlayers = (p1, p2) => {
-        firstPlayer = p1;
-        secondPlayer = p2;
-    };
-    const setCurrentPlayer = p => currentPlayer = p;
-    const getCurrentPlayer = () => currentPlayer;
-    const toggleCurrentPlayer = () => {
-        if (currentPlayer === firstPlayer) currentPlayer = secondPlayer
-        else currentPlayer = firstPlayer
-    };
-    const gameStart = () => {
-        isStart = true;
-        gameboard.clearBoard();
-        setCurrentPlayer(firstPlayer);
-    };
-    const checkResult = () => {
-        winnerIndex = gameboard.checkWinner();
-        if (winnerIndex === firstPlayer.getIndex()) {
-            winner = firstPlayer; 
-            isStart = false; 
-            resultString = `Game over! Winner is ${winner.getName()}!`;
-        }
-        if (winnerIndex === secondPlayer.getIndex()) {
-            winner = secondPlayer; 
-            isStart = false; 
-            resultString = `Game over! Winner is ${winner.getName()}!`;
-        }
-        if (winnerIndex === gameboard.getTieIndex()) { 
-            isStart = false;
-            resultString = "Game over! It's a tie!"
-        }
-    };
-    const currentPlayerMakeMove =  (axis) =>{
-        currentPlayer.makeMove(axis);
-        checkResult();
-        if (isStart) { toggleCurrentPlayer() }
-        else { console.log(resultString) }
-    };
-    const gameRestart = () => {
-        gameStart();
-    }
-
-    return {
-        gameStart,
-        setPlayers,
-        getCurrentPlayer,
-        currentPlayerMakeMove,
-        gameRestart,
-    }
-})();
-
-//testing
-
-const player1 = createPlayer("Adam", 1);
-const player2 = createPlayer("Eva", 2);
-
-gameFlow.setPlayers(player1, player2);
-gameFlow.gameStart();
-gameFlow.currentPlayerMakeMove(createAxis(1, 1));
-gameFlow.currentPlayerMakeMove(createAxis(2, 2));
-gameFlow.currentPlayerMakeMove(createAxis(0, 1));
-gameFlow.currentPlayerMakeMove(createAxis(2, 1));
-gameFlow.currentPlayerMakeMove(createAxis(2, 0));
-gameFlow.currentPlayerMakeMove(createAxis(0, 2));
-gameFlow.currentPlayerMakeMove(createAxis(0, 0));
-gameFlow.currentPlayerMakeMove(createAxis(1, 0));
-gameFlow.currentPlayerMakeMove(createAxis(1, 2));
-console.table(gameboard.currentBoard());
+    displayController.displayBoard();
+    displayController.updateForm();
+}();
